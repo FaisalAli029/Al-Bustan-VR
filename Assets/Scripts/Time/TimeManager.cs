e68b15904b4a77956e363a3ad7d6832647cbea61
FaisalAli029 <201900929@student.polytechnic.bh>
2023-10-26 23:19:49 +0300

    time and day and night cycle
    first iteration of the day and night cycle

---
+++ Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private float timeMultiplier;
+
+    [SerializeField]
+    private float startHour;
+
+    [SerializeField]
+    private Light sunLight;
+
+    [SerializeField]
+    private float sunriseHour;
+
+    [SerializeField]
+    private float sunsetHour;
+
+    private DateTime currentTime;
+
+    private TimeSpan sunriseTime;
+
+    private TimeSpan sunsetTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeOfDay();
+    }
+
+    private void UpdateTimeOfDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
+    }
+
+    private void RotateSun()
+    {
+        float sunLightRotation;
+
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime) 
+        {
+            //TimeSpan
+        }
+    }
+
+    private TimeSpan CalculateTimeDifference(TimeSpan fromTime, TimeSpan toTime)
+    {
+        TimeSpan diff = toTime - fromTime;
+
+        if (diff.TotalSeconds < 0)
+        {
+            diff += TimeSpan.FromHours(24);
+        }
+
+        return diff;
+    }
+}
