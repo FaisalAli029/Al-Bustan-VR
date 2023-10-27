8529eac364ad70962cee2ff3f08753cafbb1214e
FaisalAli029 <201900929@student.polytechnic.bh>
2023-10-27 13:01:55 +0300

    time and day and night cycle
    completed first iteration of this feature

--- Assets/Scripts/Time/TimeManager.cs
+++ Assets/Scripts/Time/TimeManager.cs
@@ -20,6 +20,24 @@ public class TimeManager : MonoBehaviour
     [SerializeField]
     private float sunsetHour;
 
+    [SerializeField]
+    private Color dayAmbientLight;
+
+    [SerializeField]
+    private Color nightAmbientLight;
+
+    [SerializeField]
+    private AnimationCurve lightChangeCurve;
+
+    [SerializeField]
+    private float maxSunLightIntensity;
+
+    [SerializeField]
+    private Light moonLight;
+
+    [SerializeField]
+    private float maxMoonLightIntensity;
+
     private DateTime currentTime;
 
     private TimeSpan sunriseTime;
@@ -30,12 +48,17 @@ public class TimeManager : MonoBehaviour
     void Start()
     {
         currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+
+        sunriseTime = TimeSpan.FromHours(sunriseHour);
+        sunsetTime = TimeSpan.FromHours(sunsetHour);
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateTimeOfDay();
+        RotateSun();
+        UpdateLightSettings();
     }
 
     private void UpdateTimeOfDay()
@@ -47,10 +70,34 @@ public class TimeManager : MonoBehaviour
     {
         float sunLightRotation;
 
-        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime) 
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime)
         {
-            //TimeSpan
+            TimeSpan sunriseToSunsetDuration = CalculateTimeDifference(sunsetTime, sunriseTime);
+            TimeSpan timeSinceSunrise = CalculateTimeDifference(sunriseTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(0, 180, (float)percentage);
         }
+        else 
+        {
+            TimeSpan sunsetToSunriseDuration = CalculateTimeDifference(sunsetTime, sunriseTime);
+            TimeSpan timeSinceSunset = CalculateTimeDifference(sunsetTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunset.TotalMinutes / sunsetToSunriseDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(180, 360, (float)percentage);
+        }
+
+        sunLight.transform.rotation = Quaternion.AngleAxis(sunLightRotation, Vector3.right);
+    }
+
+    private void UpdateLightSettings()
+    {
+        float dotProduct = Vector3.Dot(sunLight.transform.forward, Vector3.down);
+        sunLight.intensity = Mathf.Lerp(0, maxSunLightIntensity, lightChangeCurve.Evaluate(dotProduct));
+        moonLight.intensity = Mathf.Lerp(maxMoonLightIntensity, 0, lightChangeCurve.Evaluate(dotProduct));
+        RenderSettings.ambientLight = Color.Lerp(nightAmbientLight, dayAmbientLight, lightChangeCurve.Evaluate(dotProduct));
     }
 
     private TimeSpan CalculateTimeDifference(TimeSpan fromTime, TimeSpan toTime)
