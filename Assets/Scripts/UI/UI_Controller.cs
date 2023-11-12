e549238c636e66bc6c6f546842a3ab553af306d4
FaisalAli029 <201900929@student.polytechnic.bh>
2023-11-12 19:47:04 +0300

    started on inventory

--- Assets/Scripts/UI/UI_Controller.cs
+++ Assets/Scripts/UI/UI_Controller.cs
@@ -5,8 +5,26 @@ using UnityEngine;
 
 public class UI_Controller : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainMenu;
+
+    [SerializeField]
+    private GameObject optionsMenu;
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OptionsToMain()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void MainToOptions()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
     }
 }
